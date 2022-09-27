63681faf951eee02522a2afddb3e267833eb5370
Nicolas Broca <n.broca@gmail.com>
2022-09-27 11:47:17 +1000

    add byte size to readable function

--- index.js
+++ index.js
@@ -41,3 +41,37 @@ export function writeToJsonL(filepath, data) {
 
   writeFileSync(filepath, parseToJsonL(data))
 }
+
+/**
+ * This function formats a raw byte number into a readable string
+ *
+ * Adapted from the [function](https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string) from the stackoverflow user [mpen](https://stackoverflow.com/users/65387/mpen).
+ *
+ * @param {Number} bytes The integer you want to readablise
+ * @param {boolean} si `true` to use metric units (power of 1000) or `false` to use binary (IEC, power of 1024)
+ * @param {*} dp number of decimal to display
+ * @returns A formatted string
+ */
+export function byteSizeToReadable(bytes, si = false, dp = 1) {
+  const thresh = si ? 1000 : 1024
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + ' B'
+  }
+
+  const units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  let u = -1
+  const r = 10 ** dp
+
+  do {
+    bytes /= thresh
+    ++u
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  )
+
+  return bytes.toFixed(dp) + ' ' + units[u]
+}
